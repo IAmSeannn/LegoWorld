2940a0b64c7123713ff2912317542d947a4bacb0
IAmSeannn <iamseannn@gmail.com>
2016-12-31 22:14:12 +0000

    Integrated Ability to draw a rectangle into EasyDirectX.

--- Code/EasyDirectX.cpp
+++ Code/EasyDirectX.cpp
@@ -1,4 +1,10 @@
 #include "EasyDirectX.h"
+EasyDirectX::EasyDirectX()
+{
+}
+EasyDirectX::~EasyDirectX()
+{
+}
 
 //messaging function
 LRESULT WINAPI MsgProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
@@ -16,24 +22,19 @@ LRESULT WINAPI MsgProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 
 	return DefWindowProc(hWnd, msg, wParam, lParam);
 }
-EasyDirectX::EasyDirectX()
-{
-}
-EasyDirectX::~EasyDirectX()
-{
-}
 
 
-
+//-------------------------------------------------------
+//Class Functions
 void EasyDirectX::RegisterWindow()
 {
-	wc = { sizeof(WNDCLASSEX), CS_CLASSDC, MsgProc, 0L, 0L, GetModuleHandle(NULL), NULL, NULL, NULL, NULL, "Blank", NULL };
+	wc = { sizeof(WNDCLASSEX), CS_CLASSDC, MsgProc, 0L, 0L, GetModuleHandle(NULL), NULL, NULL, NULL, NULL, "LegoWorld", NULL };
 	RegisterClassEx(&wc);
 }
 
 void EasyDirectX::CreateApplicationWindow()
 {
-	hWnd = { CreateWindow("Blank", "Blank Direct3D Application", WS_OVERLAPPEDWINDOW, 100, 100, 700, 700, GetDesktopWindow(), NULL, wc.hInstance, NULL) };
+	hWnd = { CreateWindow("LegoWorld", "LegoWorld - A Place For Lego", WS_OVERLAPPEDWINDOW, 100, 100, 700, 700, GetDesktopWindow(), NULL, wc.hInstance, NULL) };
 }
 
 void EasyDirectX::InitializeDirect3D()
@@ -41,36 +42,41 @@ void EasyDirectX::InitializeDirect3D()
 	// Initialize Direct3D
 	if (SUCCEEDED(SetupD3D(hWnd)))
 	{
-		// Show the window
-		ShowWindow(hWnd, SW_SHOWDEFAULT);
-		UpdateWindow(hWnd);
-
-		// Enter the message loop
-		MSG msg;
-		ZeroMemory(&msg, sizeof(msg));
-
-		// Respond to messages until a 'WM_QUIT' message is received.
-		while (msg.message != WM_QUIT)
+		// Create the scene geometry
+		if (SUCCEEDED(SetupGeometry()))
 		{
-			if (PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE))
+			// Show the window
+			ShowWindow(hWnd, SW_SHOWDEFAULT);
+			UpdateWindow(hWnd);
+
+			// Enter the message loop
+			MSG msg;
+			ZeroMemory(&msg, sizeof(msg));
+			while (msg.message != WM_QUIT)
 			{
-				TranslateMessage(&msg);
-				DispatchMessage(&msg);
+				if (PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE))
+				{
+					TranslateMessage(&msg);
+					DispatchMessage(&msg);
+				}
+				else
+					Render();
 			}
-			else
-				Render();
 		}
 
-		CleanUp();	// Delete any resources that DX has created/used.
+		CleanUp();
 	}
 
-	UnregisterClass("Blank", wc.hInstance);
+	UnregisterClass("LegoWorld", wc.hInstance);
 }
 
 HRESULT EasyDirectX::SetupD3D(HWND hWnd)
 {
+	// Initialise Direct 3D.
+	// Requires a handle to the window in which the graphics will be drawn.
+
 	// Create the D3D object, return failure if this can't be done.
-	if (NULL == (D3D = Direct3DCreate9(D3D_SDK_VERSION))) return E_FAIL;
+	if (NULL == (pD3D = Direct3DCreate9(D3D_SDK_VERSION))) return E_FAIL;
 
 	// Set up the structure used to create the D3DDevice
 	D3DPRESENT_PARAMETERS d3dpp;
@@ -82,41 +88,138 @@ HRESULT EasyDirectX::SetupD3D(HWND hWnd)
 	d3dpp.AutoDepthStencilFormat = D3DFMT_D16;
 
 	// Create the D3DDevice
-	if (FAILED(D3D->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd,
+	if (FAILED(pD3D->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd,
 		D3DCREATE_SOFTWARE_VERTEXPROCESSING,
-		&d3dpp, &d3dDevice)))
+		&d3dpp, &pd3dDevice)))
 	{
 		return E_FAIL;
 	}
 
 	// Turn on the Z buffer
-	d3dDevice->SetRenderState(D3DRS_ZENABLE, TRUE);
+	pd3dDevice->SetRenderState(D3DRS_ZENABLE, TRUE);
+
+	// Turn off the lighting, as we're using our own vertex colours.
+	pd3dDevice->SetRenderState(D3DRS_LIGHTING, FALSE);
+
+	return S_OK;
+}
+
+HRESULT EasyDirectX::SetupGeometry()
+{
+	// Calculate the number of vertices required, and the size of the buffer to hold them.
+	int Vertices = 2 * 3;	// Six vertices for the rectangle.
+	int BufferSize = Vertices * sizeof(CUSTOMVERTEX);
+
+	// Now get Direct3D to create the vertex buffer.
+	// The vertex buffer for the rectangle doesn't exist at this point, but the pointer to
+	// it does (g_pVertexBuffer)
+	if (FAILED(pd3dDevice->CreateVertexBuffer(BufferSize, 0, D3DFVF_CUSTOMVERTEX, D3DPOOL_DEFAULT, &pVertexBuffer, NULL)))
+	{
+		return E_FAIL; // if the vertex buffer culd not be created.
+	}
+
+	// Fill the buffer with appropriate vertices to describe the rectangle.
+	// The rectangle will be made from two triangles...
+
+	// Create a pointer to the first vertex in the buffer
+	// Also lock it, so nothing else can touch it while the values are being inserted.
+	CUSTOMVERTEX* pVertices;
+	if (FAILED(pVertexBuffer->Lock(0, 0, (void**)&pVertices, 0)))
+	{
+		return E_FAIL;  // if the pointer to the vertex buffer could not be established.
+	}
+
+	// Fill the vertex buffers with data...
+
+	// Triangle 1.
+	pVertices[0].x = 0;
+	pVertices[0].y = 0;
+	pVertices[0].z = 0;
+	pVertices[0].colour = 0x00ff0000; // (red)
+
+	pVertices[1].x = 0;
+	pVertices[1].y = 10;
+	pVertices[1].z = 0;
+	pVertices[1].colour = 0x00ff0000; // (yellow)
+
+	pVertices[2].x = 10;
+	pVertices[2].y = 0;
+	pVertices[2].z = 0;
+	pVertices[2].colour = 0x00ff0000; // (green)
+
+									  // Triangle 2.
+	pVertices[3].x = 10;
+	pVertices[3].y = 0;
+	pVertices[3].z = 0;
+	pVertices[3].colour = 0x00ff0000; // (red)
+
+	pVertices[4].x = 0;
+	pVertices[4].y = 10;
+	pVertices[4].z = 0;
+	pVertices[4].colour = 0x00ffff00; // (yellow)
+
+	pVertices[5].x = 10;
+	pVertices[5].y = 10;
+	pVertices[5].z = 0;
+	pVertices[5].colour = 0x0000ff00; // (green)
+
+
+									  // Unlock the vertex buffer...
+	pVertexBuffer->Unlock();
 
 	return S_OK;
 }
 
 void EasyDirectX::Render()
 {
-	DWORD BackgroundColour = 0xF05020;
+	// Clear the backbuffer to a black color
+	pd3dDevice->Clear(0, NULL, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, D3DCOLOR_XRGB(0, 0, 150), 1.0f, 0);
 
-	// Clear the backbuffer and the z buffer.
-	d3dDevice->Clear(0, NULL, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, BackgroundColour, 1.0f, 0);
-
-	// Begin rendering the scene.
-	if (SUCCEEDED(d3dDevice->BeginScene()))
+	// Begin the scene
+	if (SUCCEEDED(pd3dDevice->BeginScene()))
 	{
-		// ...this is where the graphics would be drawn...
+		// Define the viewpoint.
+		SetupViewMatrices();
 
-		d3dDevice->EndScene();
+		// Render the contents of the vertex buffer.
+		pd3dDevice->SetStreamSource(0, pVertexBuffer, 0, sizeof(CUSTOMVERTEX));
+		pd3dDevice->SetFVF(D3DFVF_CUSTOMVERTEX);
+		pd3dDevice->DrawPrimitive(D3DPT_TRIANGLELIST, 0, 2);  // using a D3DPT_TRIANGLELIST primitive
+
+																// End the scene.
+		pd3dDevice->EndScene();
 	}
 
 	// Present the backbuffer to the display.
-	d3dDevice->Present(NULL, NULL, NULL, NULL);
+	pd3dDevice->Present(NULL, NULL, NULL, NULL);
 }
 
 void EasyDirectX::CleanUp()
 {
-	if (d3dDevice != NULL) d3dDevice->Release();
-	if (D3D != NULL)		  D3D->Release();
+	// Release (delete) all the resources used by this program.
+	// Only release things if they are valid (i.e. have a valid pointer).
+	// If not, the program will crash at this point.
+
+	if (pVertexBuffer != NULL)	pVertexBuffer->Release();
+	if (pd3dDevice != NULL)		pd3dDevice->Release();
+	if (pD3D != NULL)			pD3D->Release();
+}
+
+void EasyDirectX::SetupViewMatrices()
+{
+	// Set up the view matrix.
+	// This defines which way the 'camera' will look at, and which way is up.
+	D3DXVECTOR3 vCamera(0.0f, 0.0f, -35.0f);
+	D3DXVECTOR3 vLookat(0.0f, 0.0f, 0.0f);
+	D3DXVECTOR3 vUpVector(0.0f, 1.0f, 0.0f);
+	D3DXMATRIX matView;
+	D3DXMatrixLookAtLH(&matView, &vCamera, &vLookat, &vUpVector);
+	pd3dDevice->SetTransform(D3DTS_VIEW, &matView);
+
+	// Set up the projection matrix.
+	// This transforms 2D geometry into a 3D space.
+	D3DXMATRIX matProj;
+	D3DXMatrixPerspectiveFovLH(&matProj, D3DX_PI / 4, 1.0f, 1.0f, 100.0f);
+	pd3dDevice->SetTransform(D3DTS_PROJECTION, &matProj);
 }
 
--- Code/EasyDirectX.h
+++ Code/EasyDirectX.h
@@ -7,7 +7,15 @@
 #include <Windows.h>	// Windows library (for window functions, menus, dialog boxes, etc)
 #include <d3dx9.h>		// Direct 3D library (for all Direct 3D funtions).
 
+// A structure for our custom vertex type
+struct CUSTOMVERTEX
+{
+	float x, y, z;      // X, Y, Z position of the vertex.
+	DWORD colour;       // The vertex color
+};
 
+// The structure of a vertex in our vertex buffer...
+#define D3DFVF_CUSTOMVERTEX (D3DFVF_XYZ | D3DFVF_DIFFUSE)
 
 class EasyDirectX
 {
@@ -25,11 +33,14 @@ private:
 	//vars from WINMAIN
 	WNDCLASSEX wc;
 	HWND hWnd;
-	LPDIRECT3D9 D3D;
-	LPDIRECT3DDEVICE9 d3dDevice;
+	LPDIRECT3D9 pD3D;
+	LPDIRECT3DDEVICE9 pd3dDevice;
+	LPDIRECT3DVERTEXBUFFER9 pVertexBuffer;
 
 	void Render();
 	void CleanUp();
+	void SetupViewMatrices();
 	HRESULT SetupD3D(HWND hWnd);
+	HRESULT SetupGeometry();
 };
 
